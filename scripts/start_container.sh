95635b5802d9ce92a9aec4b958625a0020c365c3
unknown <sudhanshu.saxena0@gmail.com>
2026-03-15 15:04:55 +0530

    first

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull talktossaxena/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 talktossaxena/simple-python-flask-app
+docker run -d -p 8000:8000 talktossaxena/simple-python-flask-app
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+containerid=`docker ps | awk '{print $1}'`
+docker rm -f $containerid
